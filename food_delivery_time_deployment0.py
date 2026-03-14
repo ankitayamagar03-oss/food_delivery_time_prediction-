2c8c31f6d17561899fe4e5a2c8238c8eae9edc7e
ankitayamagar03-oss <ankitayamagar03@gmail.com>
2026-03-14 16:27:32 +0530

    Update food_delivery_time_deployment0.py

--- food_delivery_time_deployment0.py
+++ food_delivery_time_deployment0.py
@@ -7,8 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1JI6j1fesHEifI8vVsGjCHAmnt34NvMxp
 """
 
-
-    import streamlit as st
+import streamlit as st
 import pandas as pd
 import joblib
 
