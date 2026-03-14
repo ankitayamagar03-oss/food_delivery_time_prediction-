3993b764e1e467adc2314ccffff53ea5f77485fd
ankitayamagar03-oss <ankitayamagar03@gmail.com>
2026-03-14 16:30:07 +0530

    Update food_delivery_time_deployment0.py

--- food_delivery_time_deployment0.py
+++ food_delivery_time_deployment0.py
@@ -12,7 +12,7 @@ import pandas as pd
 import joblib
 
 # Load model and encoder
-model = joblib.load("food_delivery_time_prediction_xgb_model.pkl")
+model = joblib.load("food_delivery_time _prediction_xgb_model.pkl")
 encoder = joblib.load("label_encoder_food_deliverytime_prediction.pkl")
 
 st.title("Food Delivery Time Prediction Model")
